4347e8c7a52934933389b83bcb28d7e1c768ac3b
Florent Yvon <florent@ebi.ac.uk>
2014-08-15 15:46:06 +0100

    method renaming

--- libPharmML/src/eu/ddmore/libpharmml/dom/dataset/ColumnMappingType.java
+++ libPharmML/src/eu/ddmore/libpharmml/dom/dataset/ColumnMappingType.java
@@ -148,9 +148,39 @@ public class ColumnMappingType
      * Objects of the following type(s) are allowed in the list
      * {@link CategoryMapping }
      * 
+     * @deprecated Use {@link #getListOfCategoryMapping()}.
+     */
+    @Deprecated
+    public List<CategoryMapping> getCategoryMapping() {
+        if (categoryMapping == null) {
+            categoryMapping = new ArrayList<CategoryMapping>();
+        }
+        return this.categoryMapping;
+    }
+    
+    /**
+     * Gets the value of the categoryMapping property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the categoryMapping property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getCategoryMapping().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link CategoryMapping }
+     * 
      * 
      */
-    public List<CategoryMapping> getCategoryMapping() {
+    public List<CategoryMapping> getListOfCategoryMapping() {
         if (categoryMapping == null) {
             categoryMapping = new ArrayList<CategoryMapping>();
         }
