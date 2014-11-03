9521d1c6826946a33812c3ce84d05b13c390851e
Florent Yvon <florent@ebi.ac.uk>
2014-11-03 13:12:27 +0000

    completed javadoc

--- libPharmML/src/eu/ddmore/libpharmml/dom/commontypes/MatrixCellValue.java
+++ libPharmML/src/eu/ddmore/libpharmml/dom/commontypes/MatrixCellValue.java
@@ -2,9 +2,17 @@ package eu.ddmore.libpharmml.dom.commontypes;
 
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
+import eu.ddmore.libpharmml.dom.maths.Equation;
+
 /**
  * Interface for object types that can be used as value within in a matrix cell.
  * @author F. Yvon
+ * 
+ *  * Possible implementations:
+ * 		{@link Scalar},
+ * 		{@link SequenceType},
+ * 		{@link SymbolRefType},
+ * 		{@link Equation}
  */
 @XmlJavaTypeAdapter(MatrixCellValueAdapter.class)
 public interface MatrixCellValue {
