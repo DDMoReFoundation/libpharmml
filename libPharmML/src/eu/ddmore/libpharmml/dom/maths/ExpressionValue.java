e19b7c1d3394f96a0ddc492be8e2d17fea54185d
Florent Yvon <florent@ebi.ac.uk>
2015-03-31 16:48:16 +0100

    + ExpressionValue interface for Expression properties

---
+++ libPharmML/src/eu/ddmore/libpharmml/dom/maths/ExpressionValue.java
@@ -0,0 +1,11 @@
+package eu.ddmore.libpharmml.dom.maths;
+
+/**
+ * Interface implemented by any possible type of value within {@link Expression} objects, such as
+ * {@link Uniop} or {@link Piece}.
+ * @author F. Yvon
+ *
+ */
+public interface ExpressionValue {
+
+}
