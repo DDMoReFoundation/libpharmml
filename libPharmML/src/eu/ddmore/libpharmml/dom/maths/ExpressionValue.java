c8b57beeff7c110c0005399f253ab9631a19278b
Florent Yvon <florent@ebi.ac.uk>
2015-03-31 17:00:07 +0100

    + toJaxbElement method

--- libPharmML/src/eu/ddmore/libpharmml/dom/maths/ExpressionValue.java
+++ libPharmML/src/eu/ddmore/libpharmml/dom/maths/ExpressionValue.java
@@ -1,5 +1,7 @@
 package eu.ddmore.libpharmml.dom.maths;
 
+import javax.xml.bind.JAXBElement;
+
 /**
  * Interface implemented by any possible type of value within {@link Expression} objects, such as
  * {@link Uniop} or {@link Piece}.
@@ -7,5 +9,11 @@ package eu.ddmore.libpharmml.dom.maths;
  *
  */
 public interface ExpressionValue {
+	
+	/**
+	 * Converts the object to a JAXBElement for marshalling.
+	 * @return An instance of {@link JAXBElement} that contains the current object.
+	 */
+	public JAXBElement<? extends Operand> toJAXBElement();
 
 }
