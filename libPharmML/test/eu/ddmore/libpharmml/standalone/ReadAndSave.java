088ce9b4f18645887a13c85cd6f377686a2b6056
Florent Yvon <florent@ebi.ac.uk>
2015-10-01 11:03:36 +0100

    updated standalones

--- libPharmML/test/eu/ddmore/libpharmml/standalone/ReadAndSave.java
+++ libPharmML/test/eu/ddmore/libpharmml/standalone/ReadAndSave.java
@@ -3,18 +3,32 @@ package eu.ddmore.libpharmml.standalone;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Iterator;
+import java.util.logging.Level;
 
 import eu.ddmore.libpharmml.ILibPharmML;
 import eu.ddmore.libpharmml.IPharmMLResource;
+import eu.ddmore.libpharmml.IPharmMLValidator;
+import eu.ddmore.libpharmml.IValidationError;
+import eu.ddmore.libpharmml.IValidationReport;
 import eu.ddmore.libpharmml.PharmMlFactory;
+import eu.ddmore.libpharmml.impl.LoggerWrapper;
 
 public class ReadAndSave {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
+//		LoggerWrapper.getLogger().setLevel(Level.ALL);
+		
 		ILibPharmML libPharmML = PharmMlFactory.getInstance().createLibPharmML();
 		FileInputStream in = new FileInputStream(args[0]);
 		IPharmMLResource resource = libPharmML.createDomFromResource(in);
+		
+		IValidationReport report = libPharmML.getValidator().createValidationReport(resource);
+		Iterator<IValidationError> it = report.errorIterator();
+		while(it.hasNext()){
+			System.err.println(it.next());
+		}
 		
 		libPharmML.save(new FileOutputStream(args[1]), resource);
 
--- libPharmML/test/eu/ddmore/libpharmml/standalone/Validate.java
+++ libPharmML/test/eu/ddmore/libpharmml/standalone/Validate.java
@@ -1,5 +1,6 @@
 package eu.ddmore.libpharmml.standalone;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -18,30 +19,37 @@ public class Validate {
 
 	public static void main(String[] args) throws IOException {
 		
-		ILibPharmML libPharmML = PharmMlFactory.getInstance().createLibPharmML();
-		
-		LoggerWrapper.getLogger().setLevel(Level.ALL);
-		
-		String fileName = "/tmp/tst8499896253297435331xml";
-		
-		InputStream in = new FileInputStream(fileName);
-		IPharmMLResource pmlResource = libPharmML.createDomFromResource(in);
-		in.close();
-		
-		IValidationReport creationReport = pmlResource.getCreationReport();
-		print("CREATION REPORT: "+creationReport.numErrors()+" error(s)\n");
-		for(int i=1;i<=pmlResource.getCreationReport().numErrors();i++){
-			print("\t- "+pmlResource.getCreationReport().getError(i)+"\n");
-		}
-		@SuppressWarnings("unused")
-		PharmML dom = pmlResource.getDom();
+		for(String fileName : args){
+			ILibPharmML libPharmML = PharmMlFactory.getInstance().createLibPharmML();
+			
+			LoggerWrapper.getLogger().setLevel(Level.WARNING);
+			
+			File file = new File(fileName);
+			
+			print("Validating "+file.getName());
+			
+			InputStream in = new FileInputStream(file);
+			IPharmMLResource pmlResource = libPharmML.createDomFromResource(in);
+			in.close();
+			
+			IValidationReport creationReport = pmlResource.getCreationReport();
+			print("CREATION REPORT: "+creationReport.numErrors()+" error(s)\n");
+			for(int i=1;i<=pmlResource.getCreationReport().numErrors();i++){
+				print("\t- "+pmlResource.getCreationReport().getError(i)+"\n");
+			}
+			@SuppressWarnings("unused")
+			PharmML dom = pmlResource.getDom();
 
-		IPharmMLValidator validator = libPharmML.getValidator();
-		IValidationReport rpt = validator.createValidationReport(pmlResource);
-		print("VALIDATION REPORT: "+rpt.numErrors()+" error(s)\n");
-		for(int i=1;i<=rpt.numErrors();i++){
-			print("\t- "+rpt.getError(i)+"\n");
-		}	
+			IPharmMLValidator validator = libPharmML.getValidator();
+			IValidationReport rpt = validator.createValidationReport(pmlResource);
+			print("VALIDATION REPORT: "+rpt.numErrors()+" error(s)\n");
+			for(int i=1;i<=rpt.numErrors();i++){
+				print("\t- "+rpt.getError(i)+"\n");
+			}
+			
+			print("---------------------------");
+		}
+
 	}
 
 	private static void print(String string) {
