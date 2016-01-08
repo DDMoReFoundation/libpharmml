4941b6712d9e581f590725afb013e43559b42c2d
Florent Yvon <florent@ebi.ac.uk>
2016-01-08 16:22:18 +0000

    cleaning warnings

--- libPharmML/src/eu/ddmore/libpharmml/dom/probonto/Util.java
+++ libPharmML/src/eu/ddmore/libpharmml/dom/probonto/Util.java
@@ -2,7 +2,6 @@ package eu.ddmore.libpharmml.dom.probonto;
 
 import eu.ddmore.libpharmml.IErrorHandler;
 import eu.ddmore.libpharmml.dom.AbstractTreeNode;
-import eu.ddmore.libpharmml.impl.LoggerWrapper;
 
 class Util {
 	
@@ -11,7 +10,6 @@ class Util {
 		if(name != null){
 			ParameterName[] paramNames = name.requiredParameters();
 			for(ParameterName requiredParam : paramNames){
-				LoggerWrapper.getLogger().warning(requiredParam.toString());
 				if(!probOnto.containsParameter(requiredParam)){
 					errorHandler.handleError("undef", "Parameter \""+requiredParam.value()+"\" is missing in distribution +\""+name+"\".", (AbstractTreeNode) probOnto);
 				}
