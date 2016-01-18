ed1f3db84c44ea86cadc73c49ada4f241e501ad3
Florent Yvon <florent@ebi.ac.uk>
2016-01-18 14:01:20 +0000

    PharmML 0.8: now accepts conditions

--- libPharmML/src/eu/ddmore/libpharmml/dom/dataset/IgnoreLine.java
+++ libPharmML/src/eu/ddmore/libpharmml/dom/dataset/IgnoreLine.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014,2015 European Molecular Biology Laboratory,
+ * Copyright (c) 2014-2016 European Molecular Biology Laboratory,
  * Heidelberg, Germany.
  * 
  * Licensed under the Apache License, Version 2.0 (the
@@ -18,12 +18,18 @@
  ******************************************************************************/
 package eu.ddmore.libpharmml.dom.dataset;
 
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 import eu.ddmore.libpharmml.dom.commontypes.PharmMLRootType;
+import eu.ddmore.libpharmml.dom.maths.Condition;
+import eu.ddmore.libpharmml.util.ChainedList;
 
 
 /**
@@ -34,8 +40,11 @@ import eu.ddmore.libpharmml.dom.commontypes.PharmMLRootType;
  * <pre>
  * &lt;complexType name="IgnoreLineType">
  *   &lt;complexContent>
- *     &lt;extension base="{http://www.pharmml.org/pharmml/0.6/CommonTypes}PharmMLRootType">
- *       &lt;attribute name="symbol" use="required" type="{http://www.pharmml.org/pharmml/0.6/Dataset}IgnoreSymbolType" />
+ *     &lt;extension base="{http://www.pharmml.org/pharmml/0.8/CommonTypes}PharmMLRootType">
+ *       &lt;sequence>
+ *         &lt;element ref="{http://www.pharmml.org/pharmml/0.8/Maths}Condition" minOccurs="0"/>
+ *       &lt;/sequence>
+ *       &lt;attribute name="symbol" type="{http://www.pharmml.org/pharmml/0.8/Dataset}IgnoreSymbolType" />
  *     &lt;/extension>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -44,12 +53,16 @@ import eu.ddmore.libpharmml.dom.commontypes.PharmMLRootType;
  * @since PharmML 0.6 extension.
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "IgnoreLineType")
+@XmlType(name = "IgnoreLineType", propOrder = {
+	    "condition"
+	})
 public class IgnoreLine
     extends PharmMLRootType
 {
 
-    @XmlAttribute(name = "symbol", required = true)
+	@XmlElement(name = "Condition", namespace = NS_DEFAULT_MATH)
+    protected Condition condition;
+    @XmlAttribute(name = "symbol")
     protected String symbol;
     
     public IgnoreLine(){}
@@ -57,6 +70,37 @@ public class IgnoreLine
     public IgnoreLine(String symbol){
     	this.symbol = symbol;
     }
+    
+    public IgnoreLine(Condition condition){
+    	this.condition = condition;
+    }
+    
+    /**
+     * Gets the value of the condition property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Condition }
+     *   
+     * @since PharmML 0.8
+     */
+    public Condition getCondition() {
+        return condition;
+    }
+
+    /**
+     * Sets the value of the condition property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Condition }
+     *     
+     * @since PharmML 0.8
+     */
+    public void setCondition(Condition value) {
+        this.condition = value;
+    }
+
 
     /**
      * The ignore character symbol.
@@ -75,16 +119,27 @@ public class IgnoreLine
      * 
      * @param value
      *     allowed object is
-     *     {@link String } with the following pattern: <code>"[^\s]{1,3}"</code>
+     *     {@link String } with the following pattern: <code>"[^\s]{1,5}"</code>
      *     
      */
     public void setSymbol(String value) {
         this.symbol = value;
     }
     
+    public Condition createCondition(){
+    	Condition condition = new Condition();
+    	this.condition = condition;
+    	return condition;
+    }
+    
     @Override
     public String toString() {
     	return super.toString()+" symbol:"+symbol;
+    }
+    
+    @Override
+    protected List<TreeNode> listChildren() {
+    	return new ChainedList<TreeNode>(super.listChildren()).addIfNotNull(condition);
     }
 
 }
