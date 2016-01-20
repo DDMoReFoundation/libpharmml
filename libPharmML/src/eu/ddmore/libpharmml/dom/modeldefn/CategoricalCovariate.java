9798c7ccb8ac607655e0447e22c653a4ccc3a388
Florent Yvon <florent@ebi.ac.uk>
2016-01-20 17:50:18 +0000

    PharmML 0.8: + assign, distribution & realisation

--- libPharmML/src/eu/ddmore/libpharmml/dom/modeldefn/CategoricalCovariate.java
+++ libPharmML/src/eu/ddmore/libpharmml/dom/modeldefn/CategoricalCovariate.java
@@ -35,9 +35,28 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
+import eu.ddmore.libpharmml.dom.commontypes.Assignable;
+import eu.ddmore.libpharmml.dom.commontypes.Delay;
+import eu.ddmore.libpharmml.dom.commontypes.Interpolation;
+import eu.ddmore.libpharmml.dom.commontypes.Interval;
 import eu.ddmore.libpharmml.dom.commontypes.LevelReference;
+import eu.ddmore.libpharmml.dom.commontypes.Matrix;
+import eu.ddmore.libpharmml.dom.commontypes.MatrixSelector;
 import eu.ddmore.libpharmml.dom.commontypes.PharmMLRootType;
-import eu.ddmore.libpharmml.impl.XMLFilter;
+import eu.ddmore.libpharmml.dom.commontypes.Product;
+import eu.ddmore.libpharmml.dom.commontypes.Rhs;
+import eu.ddmore.libpharmml.dom.commontypes.Scalar;
+import eu.ddmore.libpharmml.dom.commontypes.Sequence;
+import eu.ddmore.libpharmml.dom.commontypes.Sum;
+import eu.ddmore.libpharmml.dom.commontypes.SymbolRef;
+import eu.ddmore.libpharmml.dom.commontypes.Vector;
+import eu.ddmore.libpharmml.dom.commontypes.VectorSelector;
+import eu.ddmore.libpharmml.dom.maths.Binop;
+import eu.ddmore.libpharmml.dom.maths.Constant;
+import eu.ddmore.libpharmml.dom.maths.FunctionCallType;
+import eu.ddmore.libpharmml.dom.maths.MatrixUniOp;
+import eu.ddmore.libpharmml.dom.maths.Piecewise;
+import eu.ddmore.libpharmml.dom.maths.Uniop;
 import eu.ddmore.libpharmml.util.ChainedList;
 
 
@@ -68,16 +87,25 @@ import eu.ddmore.libpharmml.util.ChainedList;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "CategoricalCovariateType", propOrder = {
 	"listOfVariabilityReference",
-    "listOfCategory"
+    "listOfCategory",
+    "assign",
+    "distribution",
+    "realisation"
 })
 public class CategoricalCovariate
-    extends PharmMLRootType
+    extends PharmMLRootType implements Assignable
 {
 
-	@XmlElement(name = "VariabilityReference", namespace = XMLFilter.NS_DEFAULT_CT)
+	@XmlElement(name = "VariabilityReference", namespace = NS_DEFAULT_CT)
     protected List<LevelReference> listOfVariabilityReference;
-    @XmlElement(name = "Category", required = true)
+    @XmlElement(name = "Category")
     protected List<Category> listOfCategory;
+    @XmlElement(name = "Assign", namespace = NS_DEFAULT_CT)
+    protected Rhs assign;
+    @XmlElement(name = "Distribution")
+    protected Distribution distribution;
+    @XmlElement(name = "Realisation")
+    protected Realisation realisation;
     
     /**
      * The level of random variability associated with a categorical covariate 
@@ -146,12 +174,226 @@ public class CategoricalCovariate
         }
         return this.listOfCategory;
     }
+    
+    /**
+     * Gets the value of the assign property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Rhs }
+     *     
+     * @since PharmML 0.8
+     */
+    public Rhs getAssign() {
+        return assign;
+    }
+
+    /**
+     * Sets the value of the assign property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Rhs }
+     *     
+     * @since PharmML 0.8
+     */
+    public void setAssign(Rhs value) {
+        this.assign = value;
+    }
+
+    /**
+     * Gets the value of the distribution property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Distribution }
+     *     
+     * @since PharmML 0.8
+     */
+    public Distribution getDistribution() {
+        return distribution;
+    }
+
+    /**
+     * Sets the value of the distribution property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Distribution }
+     *     
+     * @since PharmML 0.8
+     */
+    public void setDistribution(Distribution value) {
+        this.distribution = value;
+    }
+
+    /**
+     * Gets the value of the realisation property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Realisation }
+     *     
+     * @since PharmML 0.8
+     */
+    public Realisation getRealisation() {
+        return realisation;
+    }
+
+    /**
+     * Sets the value of the realisation property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Realisation }
+     *     
+     * @since PharmML 0.8
+     */
+    public void setRealisation(Realisation value) {
+        this.realisation = value;
+    }
 
 	@Override
 	protected List<TreeNode> listChildren() {
 		return new ChainedList<TreeNode>()
 				.addIfNotNull(listOfVariabilityReference)
-				.addIfNotNull(listOfCategory);
+				.addIfNotNull(listOfCategory)
+				.addIfNotNull(assign)
+				.addIfNotNull(distribution)
+				.addIfNotNull(realisation);
+	}
+	
+	 @Override
+	public Rhs assign(Scalar scalar) {
+		Rhs rhs = new Rhs(scalar);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(SymbolRef symbolRef) {
+		Rhs rhs = new Rhs(symbolRef);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Sequence sequence) {
+		Rhs rhs = new Rhs(sequence);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Vector vector) {
+		Rhs rhs = new Rhs(vector);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Interpolation interpolation) {
+		Rhs rhs = new Rhs(interpolation);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Matrix matrix) {
+		Rhs rhs = new Rhs(matrix);
+		setAssign(rhs);
+		return rhs;
+	}
+	
+	@Override
+	public Rhs assign(Constant constant) {
+		Rhs rhs = new Rhs(constant);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Interval interval) {
+		Rhs rhs = new Rhs(interval);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Binop binop) {
+		Rhs rhs = new Rhs(binop);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Uniop uniop) {
+		Rhs rhs = new Rhs(uniop);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Piecewise piecewise) {
+		Rhs rhs = new Rhs(piecewise);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(FunctionCallType functionCall) {
+		Rhs rhs = new Rhs(functionCall);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Sum sum) {
+		Rhs rhs = new Rhs(sum);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Product product) {
+		Rhs rhs = new Rhs(product);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Delay delay) {
+		Rhs rhs = new Rhs(delay);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(VectorSelector vectorSelector) {
+		Rhs rhs = new Rhs(vectorSelector);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(MatrixSelector matrixSelector) {
+		Rhs rhs = new Rhs(matrixSelector);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(MatrixUniOp matrixUniop) {
+		Rhs rhs = new Rhs(matrixUniop);
+		setAssign(rhs);
+		return rhs;
+	}
+
+	@Override
+	public Rhs assign(Probability probability) {
+		Rhs rhs = new Rhs(probability);
+		setAssign(rhs);
+		return rhs;
 	}
 
 }
