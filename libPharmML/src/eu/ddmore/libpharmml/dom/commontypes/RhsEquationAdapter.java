c6618e093f9b773b21edd4fd8861342eaa478b15
Florent Yvon <florent@ebi.ac.uk>
2016-01-28 10:56:58 +0000

    RhsEquationAdapter marshalling to Equation

--- libPharmML/src/eu/ddmore/libpharmml/dom/commontypes/RhsEquationAdapter.java
+++ libPharmML/src/eu/ddmore/libpharmml/dom/commontypes/RhsEquationAdapter.java
@@ -4,18 +4,36 @@ import static eu.ddmore.libpharmml.impl.LoggerWrapper.getLogger;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-public class RhsEquationAdapter extends XmlAdapter<Rhs, Rhs>{
+import eu.ddmore.libpharmml.dom.maths.Equation;
+import eu.ddmore.libpharmml.util.annotations.HasElementsRenamed;
+
+/**
+ * This adapter handles the backward compatibility of {@link Equation} with PharmML models encoded in version 0.6 and older.
+ * This adapter is used in combination with the annotation {@link HasElementsRenamed} in classes where the equation attribute
+ * was replaced with an assign attribute in PharmML 0.7 update.
+ * 
+ * @author F. Yvon
+ *
+ */
+@SuppressWarnings("deprecation")
+public class RhsEquationAdapter extends XmlAdapter<Equation, Rhs>{
 
 	@Override
-	public Rhs unmarshal(Rhs v) throws Exception {
-		getLogger().info("Unmarshal using RhsVoidAdapter");
-		return v;
+	public Equation marshal(Rhs v) throws Exception {
+		if(v == null){
+			return null;
+		}
+		getLogger().info("Converting Rhs to Equation");
+		return Equation.fromRhs(v);
 	}
 
 	@Override
-	public Rhs marshal(Rhs v) throws Exception {
-		getLogger().info("Marshal using RhsVoidAdapter");
-		return v;
+	public Rhs unmarshal(Equation v) throws Exception {
+		if(v == null){
+			return null;
+		}
+		getLogger().info("Converting Equation to Rhs");
+		return Equation.toRhs(v);
 	}
 
 }
