f09421701db22669b79d30b0a0de0185893aaa6d
Florent Yvon <florent@ebi.ac.uk>
2016-03-22 14:33:28 +0100

    PharmML 0.8.1: ColumnDefinition may have multiple types

--- libPharmML/test/eu/ddmore/libpharmml/dom/dataset/DatasetTest.java
+++ libPharmML/test/eu/ddmore/libpharmml/dom/dataset/DatasetTest.java
@@ -1,8 +1,14 @@
 package eu.ddmore.libpharmml.dom.dataset;
 
+import static eu.ddmore.libpharmml.AssertUtil.assertValid;
+import static org.junit.Assert.*;
+
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,14 +17,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+
 import eu.ddmore.libpharmml.ILibPharmML;
 import eu.ddmore.libpharmml.IPharmMLResource;
+import eu.ddmore.libpharmml.IValidationReport;
 import eu.ddmore.libpharmml.PharmMlFactory;
 import eu.ddmore.libpharmml.dom.PharmML;
 import eu.ddmore.libpharmml.dom.commontypes.RealValue;
 import eu.ddmore.libpharmml.dom.commontypes.StringValue;
 import eu.ddmore.libpharmml.dom.commontypes.SymbolType;
 import eu.ddmore.libpharmml.dom.trialdesign.DatasetMapping;
+import eu.ddmore.libpharmml.dom.trialdesign.Observation;
 import eu.ddmore.libpharmml.dom.trialdesign.Observations;
 import eu.ddmore.libpharmml.dom.trialdesign.TrialDesign;
 import eu.ddmore.libpharmml.impl.PharmMLVersion;
@@ -33,6 +42,9 @@ public class DatasetTest {
 	
 	private static final String TESTFILE = "testDataset.xml";
 	private IPharmMLResource unmarshalResource;
+	
+	private static final String TESTFILE_COLUMNDEF = "testColumnDefinition.xml";
+	private IPharmMLResource unmarshalColumnDef;
 	
 	public DatasetTest(PharmMLVersion version){
 		this.VERSION = version;
@@ -60,19 +72,25 @@ public class DatasetTest {
 	public static Collection<Object[]> parameters(){
 		
 		return Arrays.asList(new Object[][] {
-				{PharmMLVersion.V0_7_3}
+//				{PharmMLVersion.V0_7_3},
+				{PharmMLVersion.V0_8_1}
 		});
 	}
 	
-	@Test
-	public void testMarshal() throws Exception {
-		IPharmMLResource resource = libPharmML.createDom(VERSION);
+	public DataSet createValidDataset(){
 		PharmML dom = resource.getDom();
+		dom.createIndependentVariable("t");
 		
 		TrialDesign td = dom.createTrialDesign();
 		Observations obs = new Observations();
 		td.setObservations(obs);
 		DatasetMapping indObs = new DatasetMapping();
+		indObs.setOid("indObs");
+		
+		ColumnMapping cmap = indObs.createColumnMapping();
+		cmap.createColumnRef("time");
+		cmap.createSymbRef("t");
+		
 		obs.getListOfObservationsElements().add(indObs);
 		
 		DataSet ds = indObs.createDataSet();
@@ -81,7 +99,8 @@ public class DatasetTest {
 		def.createHeaderDefinition("h1", HeaderType.MAIN_HEADER.value(), 1);
 		def.createColumnDefinition("ID", ColumnType.ID, SymbolType.STRING, 1);
 		def.createColumnDefinition("time", ColumnType.TIME, SymbolType.REAL, 2);
-		def.createColumnDefinition("dv", ColumnType.DV, SymbolType.REAL, 3);
+		ColumnDefinition cdef = def.createColumnDefinition("dv", ColumnType.DV, SymbolType.REAL, 3);
+		cdef.setColumnType(ColumnType.DV,ColumnType.OCCASION);
 		
 		DataSetTable table = ds.createTable();
 		
@@ -98,10 +117,34 @@ public class DatasetTest {
 		row2.getListOfValue().add(new RealValue(2));
 		row2.getListOfValue().add(new RealValue(1.9));
 		
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		libPharmML.save(baos, resource);
+		libPharmML.save(System.out, resource);
+
+		return ds;
+	}
+	
+	@Test
+	public void testValidate() throws Exception {
+		createValidDataset();
 		
-		System.out.println(baos);
+		IValidationReport report = libPharmML.getValidator().createValidationReport(resource);
+		assertValid(report);
+	}
+	
+	@Test
+	public void testUnmarshalMultipleColumnTypes() throws Exception {
+		InputStream is = this.getClass().getResourceAsStream(TESTFILE_COLUMNDEF);
+		unmarshalColumnDef = libPharmML.createDomFromResource(is);
+		PharmML dom = unmarshalColumnDef.getDom();
+		TrialDesign td = dom.getTrialDesign();
+		Observations obs = td.getObservations();
+		DatasetMapping indObs = (DatasetMapping) obs.getListOfObservationsElements().get(0);
+		DataSet ds = indObs.getDataSet();
+		HeaderColumnsDefinition hcdef = ds.getDefinition();
+		ColumnDefinition cdef = hcdef.getListOfColumn().get(2);
+		List<ColumnType> types = cdef.getListOfColumnType();
+		assertEquals("2 types",2 , types.size());
+		assertEquals("type1=dv",ColumnType.DV, types.get(0));
+		assertEquals("type2=occasion",ColumnType.OCCASION, types.get(1));
 	}
 	
 }
