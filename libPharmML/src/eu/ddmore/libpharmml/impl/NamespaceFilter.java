d9f8fc009b655e4752e4a9456c6a2ba92451fa0b
Florent Yvon <florent@ebi.ac.uk>
2016-05-09 11:04:11 +0100

    added XMLStreamWriter option

--- libPharmML/src/eu/ddmore/libpharmml/impl/NamespaceFilter.java
+++ libPharmML/src/eu/ddmore/libpharmml/impl/NamespaceFilter.java
@@ -24,19 +24,28 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.Iterator;
 
 import javax.xml.namespace.NamespaceContext;
 import javax.xml.namespace.QName;
 import javax.xml.stream.Location;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.XMLFilterImpl;
 
+/**
+ * This class performs namespace URIs conversion during marshalling and unmarshalling if needed.
+ * 
+ * @author F. Yvon
+ * @see XMLFilterImpl
+ */
 public class NamespaceFilter extends XMLFilterImpl {
 	
 	static final private String NS_PATTERN_ROOT = "http://www.pharmml.org/pharmml/%s/";
@@ -48,7 +57,7 @@ public class NamespaceFilter extends XMLFilterImpl {
 	static final protected String NS_PATTERN_MSTEPS = NS_PATTERN_ROOT + "ModellingSteps";
 	static final public String NS_PATTERN_MML = NS_PATTERN_ROOT + "PharmML";
 	
-//	static final private PharmMLVersion DEFAULT_VERSION = PharmMLVersion.V0_6;
+	// Default namespace URIs. These fields MUST BE updated to the latest PharmML version
 	static final public String NS_DEFAULT_CT = "http://www.pharmml.org/pharmml/0.8/CommonTypes";
 	static final public String NS_DEFAULT_DS = "http://www.pharmml.org/pharmml/0.8/Dataset";
 	static final public String NS_DEFAULT_MATH = "http://www.pharmml.org/pharmml/0.8/Maths";
@@ -79,6 +88,10 @@ public class NamespaceFilter extends XMLFilterImpl {
 	static final public String NS_OLD_MSTEPS = NS_OLD_ROOT + "ModellingSteps";
 	static final public String NS_OLD_MML = NS_OLD_ROOT + "PharmML";
 	
+	/**
+	 * Constructs the namespace filter needed for a document with the given written version.
+	 * @param writtenVersion The {@link PharmMLVersion} of the marshalled/unmarshalled document.
+	 */
     public NamespaceFilter(PharmMLVersion writtenVersion) {
         super();
         
@@ -98,7 +111,7 @@ public class NamespaceFilter extends XMLFilterImpl {
         	if(writtenVersion.isEqualOrLaterThan(PharmMLVersion.V0_7_3)){
         		NS_DOC_PROBONTO = NS_DEFAULT_PROBONTO;
         	} else {
-        		NS_DOC_PROBONTO = NS_DOC_MSTEPS;
+        		NS_DOC_PROBONTO = NS_DOC_MSTEPS; // In 0.7, ProbOnto elements were located in the msteps schema 
         	}
         	
         } else {
@@ -143,7 +156,7 @@ public class NamespaceFilter extends XMLFilterImpl {
      * @param ns Document namespace
      * @return Default namespace
      */
-    protected String filterInputNamespace(String ns){
+    private String filterInputNamespace(String ns){
     	if(ns == null){
     		return null;
     	}
@@ -167,6 +180,35 @@ public class NamespaceFilter extends XMLFilterImpl {
     }
     
     /**
+     * Transforms the given default namespace to the one corresponding to the document version used 
+     * to construct this {@link NamespaceFilter}.
+     * @param ns One of the default PharmML namespaces (latest version)
+     * @return The namespace URI as written in the given document version.
+     */
+    private String filterOutputNamespace(String ns){
+    	if(ns == null){
+    		return null;
+    	}
+    	if(ns.equals(NS_DEFAULT_CT)){
+    		return NS_DOC_CT;
+    	} else if(ns.equals(NS_DEFAULT_DS)){
+        	return NS_DOC_DS;
+    	} else if(ns.equals(NS_DEFAULT_MATH)){
+        	return NS_DOC_MATH;
+    	} else if(ns.equals(NS_DEFAULT_MDEF)){
+        	return NS_DOC_MDEF;
+    	} else if(ns.equals(NS_DEFAULT_MSTEPS)){
+        	return NS_DOC_MSTEPS;
+    	} else if(ns.equals(NS_DEFAULT_TD)){
+        	return NS_DOC_TD;
+    	} else if(ns.equals(NS_DEFAULT_MML)){
+        	return NS_DOC_MML;
+    	} else {
+    		return ns;
+    	}
+    }
+    
+    /**
      * Converts the given input stream to a filtered stream reader. The input stream must
      * be a model encoded with the version used to construct this {@link XMLFilter}. The output
      * of this method is the same model, but with the namespaces changed to the default ones, i.e.
@@ -175,7 +217,7 @@ public class NamespaceFilter extends XMLFilterImpl {
      * @return {@link XMLStreamReader} implementation resulting of the namespace filtering of the input model.
      * @throws XMLStreamException If error in parsing.
      */
-    public XMLStreamReader getXMLStreamReader(final InputStream is) throws XMLStreamException{
+    public final XMLStreamReader getXMLStreamReader(final InputStream is) throws XMLStreamException{
     	
     	XMLStreamReader filterReader = new XMLStreamReader(){
     		
@@ -388,6 +430,326 @@ public class NamespaceFilter extends XMLFilterImpl {
     	};
     	
     	return filterReader;
+    }
+    
+    public final XMLStreamWriter getXMLStreamWriter(final XMLStreamWriter writer) throws XMLStreamException{
+    	
+    	return new XMLStreamWriter() {
+			
+			/**
+			 * @param localName
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeStartElement(java.lang.String)
+			 */
+			public void writeStartElement(String localName) throws XMLStreamException {
+				writer.writeStartElement(localName);
+			}
+
+			/**
+			 * @param namespaceURI
+			 * @param localName
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeStartElement(java.lang.String, java.lang.String)
+			 */
+			public void writeStartElement(String namespaceURI, String localName) throws XMLStreamException {
+				writer.writeStartElement(filterOutputNamespace(namespaceURI), localName);
+			}
+
+			/**
+			 * @param namespaceURI
+			 * @param localName
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeEmptyElement(java.lang.String, java.lang.String)
+			 */
+			public void writeEmptyElement(String namespaceURI, String localName) throws XMLStreamException {
+				writer.writeEmptyElement(filterOutputNamespace(namespaceURI), localName);
+			}
+
+			/**
+			 * @param prefix
+			 * @param localName
+			 * @param namespaceURI
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeEmptyElement(java.lang.String, java.lang.String, java.lang.String)
+			 */
+			public void writeEmptyElement(String prefix, String localName, String namespaceURI)
+					throws XMLStreamException {
+				writer.writeEmptyElement(prefix, localName, filterOutputNamespace(namespaceURI));
+			}
+
+			/**
+			 * @param localName
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeEmptyElement(java.lang.String)
+			 */
+			public void writeEmptyElement(String localName) throws XMLStreamException {
+				writer.writeEmptyElement(localName);
+			}
+
+			/**
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeEndElement()
+			 */
+			public void writeEndElement() throws XMLStreamException {
+				writer.writeEndElement();
+			}
+
+			/**
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeEndDocument()
+			 */
+			public void writeEndDocument() throws XMLStreamException {
+				writer.writeEndDocument();
+			}
+
+			/**
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#close()
+			 */
+			public void close() throws XMLStreamException {
+				writer.close();
+			}
+
+			/**
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#flush()
+			 */
+			public void flush() throws XMLStreamException {
+				writer.flush();
+			}
+
+			/**
+			 * @param localName
+			 * @param value
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeAttribute(java.lang.String, java.lang.String)
+			 */
+			public void writeAttribute(String localName, String value) throws XMLStreamException {
+				writer.writeAttribute(localName, value);
+			}
+
+			/**
+			 * @param prefix
+			 * @param namespaceURI
+			 * @param localName
+			 * @param value
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeAttribute(java.lang.String, java.lang.String, java.lang.String, java.lang.String)
+			 */
+			public void writeAttribute(String prefix, String namespaceURI, String localName, String value)
+					throws XMLStreamException {
+				writer.writeAttribute(prefix, filterOutputNamespace(namespaceURI), localName, value);
+			}
+
+			/**
+			 * @param namespaceURI
+			 * @param localName
+			 * @param value
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeAttribute(java.lang.String, java.lang.String, java.lang.String)
+			 */
+			public void writeAttribute(String namespaceURI, String localName, String value) throws XMLStreamException {
+				writer.writeAttribute(filterOutputNamespace(namespaceURI), localName, value);
+			}
+
+			/**
+			 * @param prefix
+			 * @param namespaceURI
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeNamespace(java.lang.String, java.lang.String)
+			 */
+			public void writeNamespace(String prefix, String namespaceURI) throws XMLStreamException {
+				writer.writeNamespace(prefix, filterOutputNamespace(namespaceURI));
+			}
+
+			/**
+			 * @param namespaceURI
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeDefaultNamespace(java.lang.String)
+			 */
+			public void writeDefaultNamespace(String namespaceURI) throws XMLStreamException {
+				writer.writeDefaultNamespace(filterOutputNamespace(namespaceURI));
+			}
+
+			/**
+			 * @param data
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeComment(java.lang.String)
+			 */
+			public void writeComment(String data) throws XMLStreamException {
+				writer.writeComment(data);
+			}
+
+			/**
+			 * @param target
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeProcessingInstruction(java.lang.String)
+			 */
+			public void writeProcessingInstruction(String target) throws XMLStreamException {
+				writer.writeProcessingInstruction(target);
+			}
+
+			/**
+			 * @param target
+			 * @param data
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeProcessingInstruction(java.lang.String, java.lang.String)
+			 */
+			public void writeProcessingInstruction(String target, String data) throws XMLStreamException {
+				writer.writeProcessingInstruction(target, data);
+			}
+
+			/**
+			 * @param data
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeCData(java.lang.String)
+			 */
+			public void writeCData(String data) throws XMLStreamException {
+				writer.writeCData(data);
+			}
+
+			/**
+			 * @param dtd
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeDTD(java.lang.String)
+			 */
+			public void writeDTD(String dtd) throws XMLStreamException {
+				writer.writeDTD(dtd);
+			}
+
+			/**
+			 * @param name
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeEntityRef(java.lang.String)
+			 */
+			public void writeEntityRef(String name) throws XMLStreamException {
+				writer.writeEntityRef(name);
+			}
+
+			/**
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeStartDocument()
+			 */
+			public void writeStartDocument() throws XMLStreamException {
+				writer.writeStartDocument();
+			}
+
+			/**
+			 * @param version
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeStartDocument(java.lang.String)
+			 */
+			public void writeStartDocument(String version) throws XMLStreamException {
+				writer.writeStartDocument(version);
+			}
+
+			/**
+			 * @param encoding
+			 * @param version
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeStartDocument(java.lang.String, java.lang.String)
+			 */
+			public void writeStartDocument(String encoding, String version) throws XMLStreamException {
+				writer.writeStartDocument(encoding, version);
+			}
+
+			/**
+			 * @param text
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeCharacters(java.lang.String)
+			 */
+			public void writeCharacters(String text) throws XMLStreamException {
+				writer.writeCharacters(text);
+			}
+
+			/**
+			 * @param text
+			 * @param start
+			 * @param len
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#writeCharacters(char[], int, int)
+			 */
+			public void writeCharacters(char[] text, int start, int len) throws XMLStreamException {
+				writer.writeCharacters(text, start, len);
+			}
+
+			/**
+			 * @param uri
+			 * @return
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#getPrefix(java.lang.String)
+			 */
+			public String getPrefix(String uri) throws XMLStreamException {
+				return writer.getPrefix(filterOutputNamespace(uri));
+			}
+
+			/**
+			 * @param prefix
+			 * @param uri
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#setPrefix(java.lang.String, java.lang.String)
+			 */
+			public void setPrefix(String prefix, String uri) throws XMLStreamException {
+				writer.setPrefix(prefix, filterOutputNamespace(uri));
+			}
+
+			/**
+			 * @param uri
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#setDefaultNamespace(java.lang.String)
+			 */
+			public void setDefaultNamespace(String uri) throws XMLStreamException {
+				writer.setDefaultNamespace(filterOutputNamespace(uri));
+			}
+
+			/**
+			 * @param context
+			 * @throws XMLStreamException
+			 * @see javax.xml.stream.XMLStreamWriter#setNamespaceContext(javax.xml.namespace.NamespaceContext)
+			 */
+			public void setNamespaceContext(NamespaceContext context) throws XMLStreamException {
+				writer.setNamespaceContext(context);
+			}
+
+			/**
+			 * @return
+			 * @see javax.xml.stream.XMLStreamWriter#getNamespaceContext()
+			 */
+			public NamespaceContext getNamespaceContext() {
+				return writer.getNamespaceContext();
+			}
+
+			/**
+			 * @param name
+			 * @return
+			 * @throws IllegalArgumentException
+			 * @see javax.xml.stream.XMLStreamWriter#getProperty(java.lang.String)
+			 */
+			public Object getProperty(String name) throws IllegalArgumentException {
+				return writer.getProperty(name);
+			}
+
+			@Override
+			public void writeStartElement(String prefix, String localName, String namespaceURI) throws XMLStreamException {
+				writer.writeStartElement(prefix, localName, filterOutputNamespace(namespaceURI));
+			}
+			
+		};
+    }
+    
+    /**
+     * Creates a filtering {@link XMLStreamWriter} which will write the proper namespaces URIs for the given
+     * document version.
+     * @param os Output of the filtered XML.
+     * @return {@link XMLStreamWriter} implementation which will dynamically change the namespaces.
+     * @throws XMLStreamException If error in writing.
+     */
+    public final XMLStreamWriter getXMLStreamWriter(OutputStream os) throws XMLStreamException{
+    	return getXMLStreamWriter(XMLOutputFactory.newInstance().createXMLStreamWriter(os));
+    }
+    
+    public final XMLStreamWriter getXMLStreamWriter(Writer writer) throws XMLStreamException{
+    	return getXMLStreamWriter(XMLOutputFactory.newInstance().createXMLStreamWriter(writer));
     }
     
     void filterRawText(InputStream is, OutputStream os) throws IOException{
